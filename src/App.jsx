3897930c9be314b4b7c729ec7c20629c4d8f9055
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-12-25 12:04:09 +0530

    Styling added for task list

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,15 @@ function App() {
     "Complete React Project",
     "Read a Book",
     "Grocery Shopping",
+    "Plan Weekend Trip",
+    "Go for Gym",
+    "Complete React Project",
+    "Read a Book",
+    "Grocery Shopping",
     "Plan Weekend Trip"
+    
   ])
+  const [newTask, setNewTask] = useState("")
 
   return (
     <div className='home-container'>
@@ -19,16 +26,28 @@ function App() {
       <p className='app-subheading'>Manage your task efficiently with this simple ToDo App.
         <span className='highlighted-text'>No Stuff</span>, just pure productivity!
       </p>
+      <div className="task-list">
       {tasks.map((task, index) => (
         <TaskCard key={index} task={task} />
       ))}
+      </div>
       <div className="input-section">
         <input 
           type="text" 
           className="task-input" 
           placeholder="Enter a new task..." 
+          value={newTask} 
+          onChange={(e) => setNewTask(e.target.value)}
         />
-        <button className="add-task-button">Add Task</button>
+        <button 
+        className="add-task-button"
+        onClick={() => {
+          
+            setTasks([...tasks, newTask])
+            setNewTask("")
+          
+        }}
+        >Add Task</button>
       </div>
     </div>
   )
