425276875209746f1c37a55e3a58a91fb74dfd40
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-12-31 15:15:10 +0530

    App.css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,14 @@
-import { useState } from 'react'
-import TaskCard from './TaskCard'
-
+import React from 'react'
 import './App.css'
 
 function App() {
-  
-  const [tasks, setTasks] = useState([
-    "Go for Gym",
-    "Complete React Project",
-    "Read a Book",
-    "Grocery Shopping",
-    "Plan Weekend Trip",
-    "Go for Gym",
-    "Complete React Project",
-    "Read a Book",
-    "Grocery Shopping",
-    "Plan Weekend Trip"
-    
-  ])
-  const [newTask, setNewTask] = useState("")
-
   return (
-    <div className='home-container'>
-      <h1 className="app-heading">To-Do Application</h1>
-      <p className='app-subheading'>Manage your task efficiently with this simple ToDo App.
-        <span className='highlighted-text'>No Stuff</span>, just pure productivity!
-      </p>
-      <div className="task-list">
-      {tasks.map((task, index) => (
-        <TaskCard key={index} task={task} />
-      ))}
-      </div>
-      <div className="input-section">
-        <input 
-          type="text" 
-          className="task-input" 
-          placeholder="Enter a new task..." 
-          value={newTask} 
-          onChange={(e) => setNewTask(e.target.value)}
-        />
-        <button 
-        className="add-task-button"
-        onClick={() => {
-          
-            setTasks([...tasks, newTask])
-            setNewTask("")
-          
-        }}
-        >Add Task</button>
-      </div>
+    <div>
+      <h1 className="title">To Do Application</h1>
+      <p className="description">Manage your tasks efficiently</p>
     </div>
   )
 }
 
 export default App
+
